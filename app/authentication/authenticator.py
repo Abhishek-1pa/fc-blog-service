87ab68830de95801bfc0b8fe4ee5d849748ac740
frozenmafia <shivajay295@gmail.com>
2024-03-01 23:41:18 +0530

    deploy

--- app/authentication/authenticator.py
+++ app/authentication/authenticator.py
@@ -1,16 +1,19 @@
+import os
+import yaml
 import httpx
-from fastapi import Depends, HTTPException , status
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from .. import models
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-IAM_SERVICE_DEV = "http://localhost:8000/get_current_user"
-IAM_SERVICE_PROD = "https://iam.forge-code.com/get_current_user"
+
+IAM_SERVICE_URL = os.getenv("IAM_SERVICE_URL")
+
 
 async def authenticate_with_iam(token: str):
     async with httpx.AsyncClient() as client:
         headers = {"Authorization": f"Bearer {token}"}
-        response = await client.post(IAM_SERVICE_PROD, headers=headers)
+        response = await client.post(f'{IAM_SERVICE_URL}/get_current_user', headers=headers)
         if response.status_code != 200:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
         
--- app/database.py
+++ app/database.py
@@ -1,13 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL_PROD = 'postgresql://postgres:123qwe123qwe@35.238.209.92/postgres'
-#SQLALCHEMY_DATABASE_URL_DEV = 'postgresql://postgres:123qwe123qwe@localhost/forgecode'
+# SQLALCHEMY_DATABASE_URL_PROD = 'postgresql://postgres:123qwe123qwe@35.238.209.92/postgres'
+# #SQLALCHEMY_DATABASE_URL_DEV = 'postgresql://postgres:123qwe123qwe@localhost/forgecode'
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL_PROD
-)
+# if "DATABASE_URL" in os.environ:
+#     SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
+# else:
+#     SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:123qwe123qwe@localhost/forgecode'
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+print(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
 
